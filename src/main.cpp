3e951db569fabfd47ed9532f5ada8438e9009867
刘在丰 <705152063@qq.com>
2026-03-21 16:00:25 +0800

    6.01

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,22 @@
 #define PWM3_PIN 6
 #define PWM4_PIN 7
 
-
-
-//全局h变量
+//全局变量
 RobotPose currentPose = {0, 0, 0, 0};//当前机器人位置
+bool isdebug = true;//是否调试模式
 
 //位置坐标-电机脉冲转换系数（mm-脉冲）
 const float X_PULSE = 18.0f;
 const float Y_PULSE = 18.0f;
 const float THETA_PULSE = 18.0f;
+
+// ================= 函数声明 =================
+
+void Task_MainStateMachine(void *pvParameters);//主状态机任务函数（正常运行模式）
+void Task_Main_Serial0_CMD(void *pvParameters);//主串口0命令任务函数
+void Task_DebugStateMachine(void *pvParameters);//调试模式状态机任务函数（调试模式）
+void Task_Debug_Serial0_CMD(void *pvParameters);//调试模式串口0命令任务函数
+
 
 // --------------------------------------------------------
 // 初始化设置
@@ -44,6 +51,17 @@ void setup() {
   initLidar();
   // 初始化电机
   Emm_V5_Init();
+
+  //开启程序
+  if (isdebug) {
+    Serial.println("Debug mode ");//调试模式 开启对应任务
+    xTaskCreate(Task_DebugStateMachine, "Task_DebugStateMachine", 2048, NULL, 5, NULL);
+    xTaskCreate(Task_Debug_Serial0_CMD, "Task_Debug_Serial0_CMD", 2048, NULL, 5, NULL);
+  } else {
+    Serial.println("Release mode ");//正常运行 开启对应任务
+    xTaskCreate(Task_MainStateMachine, "Task_MainStateMachine", 2048, NULL, 5, NULL);
+    xTaskCreate(Task_Main_Serial0_CMD, "Task_Main_Serial0_CMD", 2048, NULL, 5, NULL);
+  }
 
   Serial.println("Supermarket robot initialized");
 
